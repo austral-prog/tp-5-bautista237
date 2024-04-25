28fde4dfd206931d065330d42411d0e4d99d5522
bautista237 <162723056+bautista237@users.noreply.github.com>
2024-04-25 17:35:14 -0300

    Update maximums.py

--- maximums.py
+++ maximums.py
@@ -1,21 +1,17 @@
-# Replace the "ANSWER HERE" for your answer
 def max_of_two(x, y):
-    def max_of_two(x, y):
-        if x > y:
-            return x
-
-    elif x < y:
-    return y
-    if x == y:
-        return x or y
+	if x>y:
+		return x
+	elif x<y:
+		return y
+	if x==y:
+		return x or y
 
 def max_of_three(x, y, z):
-        if (x > y) and (x > z):
-            return x
-
-    elif (y > x) and (y > z):
-    return y
-    elif (z > x) and (z > y):
-    return z
-    if x == y == z:
-        return x or y or z
+	if (x>y) and (x>z):
+		return x
+	elif (y>x) and (y>z):
+		return y
+	elif (z>x) and (z>y):
+		return z
+	if x==y==z:
+		return x or y or z
