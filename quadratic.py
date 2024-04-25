c2c1ce2e756b153144c5805bd5436b9bafe47b7a
bautista237 <162723056+bautista237@users.noreply.github.com>
2024-04-25 17:53:09 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,20 +1,18 @@
 def roots(a, b, c):
-    r1 = ((-b+((b2)-4ac)0.5)/2a)
-    r2 = ((-b-((b**2)-4ac)**0.5)/2a)
+    r1 = ((-b+((b**2)-4*a*c)**0.5)/2*a)
+    r2 = ((-b-((b**2)-4*a*c)**0.5)/2*a)
 
-    if  (b*2)-4ac<0:
+    if  (b**2)-4*a*c<0:
         return "( )"
     elif r1!=r2:
         return f"({r1}, {r2})"
     elif r1==r2:
         return f"({r1})"
-
-
+        
 def value_y(a, b, c, x):
-    t1 = axx+bx+c
+    t1 = a*x*x+b*x+c
     return t1
-
-
+    
 def to_string(a, b, c):
     A=a 
     B=b
@@ -28,13 +26,16 @@ def to_string(a, b, c):
         return f"f(x) = {B} * X + {C}"
     else:
         return f"f(x) = {C}"
-
+        
 def derivation(a, b,c):
     if a!=0 and b!=0:
-        return f"f'(x) = {2a} X + {b}"
+        return f"f'(x) = {2*a} * X + {b}"
     elif a==0 and b!=0:
         return f"f'(x) = {b}"
     elif a==0 and b==0:
+        return f"f'(x) = 0"
+    elif a!=0 and b==0:
+        return f"f'(x) = {2*a} * X"
         return f"f'(x) = 0"
     elif a!=0 and b==0:
         return f"f'(x) = {2a} X"
